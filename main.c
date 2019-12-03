3c5cc93f31e4f5152b6d19a3edf1fab0e0df80a0
Tannerpalin <tcpa227@g.uky.edu>
2019-12-03 18:39:12 -0500

    Working example in exampleServer folder.

--- main.c
+++ main.c
@@ -8,7 +8,7 @@ struct requestType {
     unsigned int secretKey;         // unsigned, positive, secret request key.
     int requestStatus;              // Simple success or failure of request.
     char machineName[40];
-
+    
 } clientRequest;
 
 int main(/* command line arguments here? */) {
